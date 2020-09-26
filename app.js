620d87b1b9a0aa28c9648c5baf55d3ca7530f50b
Rachel Fuller <rlfuller@gmail.com>
2020-09-26 11:48:39 -0400

    Simplify creating html elements

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 
-    // Create Dino Constructor
-    function Dinosaur(dinoObject){
+    function Animal(obj) {
         this.species = dinoObject.species;
         this.weight = dinoObject.weight;
         this.height = dinoObject.height;
+        this.diet = dinoObject.diet;
+    }
+
+    // Create Dino Constructor
+    function Dinosaur(dinoObject) {
         this.where = dinoObject.where;
         this.when = dinoObject.when;
         this.fact = dinoObject.fact;
     }
-
+    Dinosaur.prototype = Object.create(Animal.prototype);
 
     // Create Dino Objects
     function constructDinosaurs(dinoArray){
-        dinoArray.array.forEach(dino => {
-            new Dinosaur(dino);
-            console.log
+        return dinoArray.map(dino => {
+            return new Dinosaur(dino);
         });
     }
 
     // Create Human Object
+
+    function Human(obj) {
+        this.name = obj.name;
+        this.height = obj.height;
+        this.
+    }
 
     // Use IIFE to get human data from form
 
@@ -38,50 +47,30 @@
     // Generate Tiles for each Dino in Array
   
         // Add tiles to DOM
-
+    const generateHtmlDinosaurs = (dinoArr) => {
         let fragment = document.createDocumentFragment();
 
-        //create div
-        let div = document.createElement("div")
-        let divAtt = document.createAttribute("class");
-        divAtt.value = "grid-item";
-        div.setAttribute(divAtt);
+        dinoArray.forEach(dino => {
 
-        //create h3
-        let h3 = document.createElement("h3");
-        let h3Text = document.createTextNode(`${obj.species}`);
-        h3.appendChild(h3Text);
+            //create div add a class and an id
+            let div = document.createElement("div");
+            div.classList.add("grid-item");
+            div.id = dino.species.toLowerCase().replace(/\s+/g, "-");
 
-        //create image
-        let img = document.createElement("img");
-        let imgAtt = document.createAttribute("src");
-        imgAtt.value = `images/${obj.species}.png`;
-        img.setAttribute = imgAtt;
+            div.innerHTML = `
+                <h3>${dino.species}</h3>
+                <img src="images/${dino.species.toLowerCase()}.png">
+                <p>${dino.fact}</p>
+            `;
+
+            //add div to the fragment
+            fragment.appendChild(div);
+        });
         
-
-        //create p
-        let p = document.createElement("p");
-        let pText = document.createTextNode(`${obj.fact}`);
-        p.appendChild(pText);
-
-        //add elements to the div
-        div.appendChild(h3);
-        div.appendChild(img);
-        div.appendChild(p);
-
-        //add div to the fragment
-        fragment.appendChild(div);
-
-        /*
-        <div class="grid-item">
-            <h3>Anklyosaurus</h3>
-            <img src="images/anklyosaurus.png">
-            
-            <p>Anklyosaurus survived for approximately 135 million years.</p>
-
-            <p>Chester is a foo!</p>
-        </div>
-        */
+        //add dinos to page
+        document.getElementById("grid").appendChild(fragment);
+    }
+        
 
 
 
@@ -95,7 +84,7 @@
 
 // On button click, prepare and display infographic
 
-const constructDinosaurs = (data) => {
+const constructDinosaurs = (dinos) => {
     console.log(data);
     //also here, construct grid objects
     //get the main tag id=grid
@@ -105,6 +94,9 @@ const constructDinosaurs = (data) => {
     //the images are not in the json
     //so get the species val, make itlower case
     //construt an image tag 
+    return dinoArray.map(dino => {
+        return new Dinosaur(dino);
+    });
     
 }
 
@@ -116,7 +108,7 @@ const getDinoData = () => {
         }
         return response.json();
     }).then((data) => {
-        constructDinosaurs(data);
+        constructDinosaurs(data.Dinos);
     }).catch((err) => {
         console.log("error", err);
 
